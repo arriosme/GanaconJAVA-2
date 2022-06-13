d7126ebb72081b4dcd8de2c919c6e35805cd9a84
arriosme <arios@novopangea.com.co>
2022-06-12 20:12:46 -0500

    Desde consola git

--- login.js
+++ login.js
@@ -2,3 +2,7 @@ Iniciamos cambios en el LOGIN
 VALIDAR CAMBIOS POR TODOS 
 CONFIRMACION CONSOLA GIT
 *****Validar consola GIT*****
+
+Hacemos cambios desde la consola GIT
+Y esperamso CONFIRMACION
+*****confirmar desde GH*******
