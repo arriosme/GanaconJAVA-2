1ff4608f9c1d56a274148d45942683a7c0c5b269
arriosme <arios@novopangea.com.co>
2022-06-12 19:49:12 -0500

    Confirmacion MASTER

--- Player.js
+++ Player.js
@@ -3,3 +3,5 @@ vamos a contruir el codigo del jugador
 por medio de archivos JS
 y lo vamos a combinar con HTMLAllCollection
 y vamos a implementar intefaz grafica
+Confirmo cambios desde el MediaStreamAudioDestinationNode
+PORQUE necestiamos establecer comunicacion
